28f3c55991836330696a6eceeaa8c53c76ed57ac
bobby3003 <roberto3003@riseup.net>
2021-11-13 16:41:19 +0700

    all

---
+++ writing_own_modules/robertomodule.py
@@ -0,0 +1,9 @@
+def func():
+    print("Roberto module direct")
+def func2():
+    print("Roberto module imported")
+
+if __name__ == "__main__":
+    func()
+else:
+    func2()
---
+++ writing_own_modules/robertoproject.py
@@ -0,0 +1,11 @@
+# from robertomodule import func
+# func()
+# same as:
+
+# import robertomodule
+# robertomodule.func()
+
+import robertomodule
+
+print("roberto")
+
